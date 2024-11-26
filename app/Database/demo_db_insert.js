9d059a91c5ddaf8749f3b921e396d81e6addcc8e
Kearskill <kearskill@gmail.com>
2024-11-26 17:25:03 +0800

    database gaming

---
+++ app/Database/demo_db_insert.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'localpassword',
+    database: 'skibidisigma',
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected successfully.");
+    var sql = "INSERT INTO customers (name,address) VALUES ('Skibidi sigma','Mountain 21')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted!");
+    })
+});
