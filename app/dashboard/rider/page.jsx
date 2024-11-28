2f20bbb4527b57eea91692020874efa48e738b9d
Akram Hilmi <akramhilh@outlook.com>
2024-11-28 13:54:23 +0800

    Update

--- app/dashboard/rider/page.jsx
+++ app/dashboard/rider/page.jsx
@@ -1,14 +1,28 @@
-import styles from "./page.module.css"
-import Card from "../components/Card.jsx"
+"use client";
+import React from "react";
+import styles from "./page.module.css";
+import Card from "../components/Card";
+import RiderPicture from "../assets/rider.png";
 
-function dashboardRider(props) {
+function Dashboard(props) {
     return (
         <div className={styles.dashboard}>
-            <h1>Hi {props.name}</h1>
-            <h2>Your earnings: RM {props.earnings}</h2>
-            <Card title="Hello world" description="Hi"/>
+            <header className={styles.header}>
+                <h1>Hello, {props.name}</h1>
+                <img className={styles.riderPicture} src={RiderPicture}/>
+                <p></p>
+            </header>
+
+            <main className={styles.content}>
+                <div className={styles.grid}>
+                    <Card title="YOUR EARNINGS" content={props.earnings}/>
+                    <Card title="STATUS" content={props.status ? "Online" : "Offline"}/>
+                    <Card title="INCOMING ORDERS" content={props.orders}/>
+
+                </div>
+            </main>
         </div>
     );
-}
+};
 
-export default dashboardRider
+export default Dashboard;
